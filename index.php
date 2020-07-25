913e70bc341680658e83b1e7f27b50dc5a38b0c1
Arakaza <aarakaza7@gmail.com>
2020-07-25 11:07:11 +0200

    add table

--- index.php
+++ index.php
@@ -37,62 +37,7 @@ $enfant=[ 'nom' => 'Ajeneza',
  
 ];
 echo 'son nom est ' .$enfant['nom'] . ' et son prenom '. $enfant['prrenom'];
-//============================Structure conditionnelle========================
-
-//================condition if=============
-$age=15;
-
-if($age >= 15){
-
-    echo 'tu es adulte </br>';
-}
-else {
-
-    echo 'tu es un enfant</br>';
-}
-
-//******switch case */
-    $ville="buja";
-    switch($ville){
-
-            case "buja":
-                echo "le capitale du burundi </br>";
-            break;
-            case "gitega":
-                echo "capitale politique du burundi</br>";
-            break;
-            default:
-                echo "pas d\'autre capitale </br>";
-            break;
-
-
-    }
-    //=======================les boucles=================================
-
-//========boucle while=======
-
-$number=0;
-while($number < 5){
-
-        echo '<p>'.$number.'</p>'; // afficher tous les nombres inferieur a 10
-
-        $number++; //parcourir toutes les valeurs inferieur de 10
-}
-//====boucle do while=====
-
-    do{
-
-        echo'<p>' .$number.'</p>';
-        $number++;
-
-    }while($number < 5); 
-
-//======boucle for=======
-for($i=0;$i<2;$i++)
-{
-    echo 'bonjour tous le monde'; 
-    
-}
+//=========================
 
     ?>
 </body>
